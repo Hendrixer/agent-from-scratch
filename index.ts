3fa24529863993260842bcc72fd1712b3298c227
Ankit <ankitkumar.lnmiit@gmail.com>
2024-11-27 17:49:22 +0530

    Agents, prompts and llms

--- index.ts
+++ index.ts
@@ -1,8 +1,13 @@
 import 'dotenv/config'
+import { runAgent  } from './src/agent'
+import { tools } from './src/tools'
 
 const userMessage = process.argv[2]
+
 
 if (!userMessage) {
   console.error('Please provide a message')
   process.exit(1)
 }
+
+await runAgent({userMessage, tools })
